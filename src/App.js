ee8b81458d880a994e62939eccf3ca883f06a15b
Lucas Gallo <lucasgallo@gmail.com>
2023-11-06 18:36:33 -0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import ExampleComponent from "./components/ExampleComponent/ExampleComponent"; // Suponiendo que ExampleComponent está en el mismo directorio
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.scss";
 
 const App = () => {
+
+  const [show, setShow] = useState(true)
+  
+  const showCounter = () => {
+    setShow(!show)
+  }
+
   return (
     <div className="App">
-      <h1>Counter</h1>
-        <ExampleComponent />
+
+      <button className="btn btn-primary" onClick={showCounter}>SHOW/HIDE</button>
+
+      {
+        show ?
+          <div className="counter-title">
+            <h2 style={{ fontSize: "50px" }}>Counter</h2>
+            <ExampleComponent />
+          </div>
+          :
+          <div></div>
+      }
+
+
+
     </div>
   );
 };
--- src/components/ExampleComponent/ExampleComponent.js
+++ src/components/ExampleComponent/ExampleComponent.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import "./ExampleComponent.scss";
 
@@ -14,16 +14,45 @@ const ExampleComponent = () => {
     setCount(initialState);
   };
 
+  // El efecto se va a ejecutar con el montaje y con cada actualización
+  /* useEffect(() => {
+    console.log("mounted component")
+  })
+   */
+
+  // El efecto se va a ejecutar sólo con el montaje
+  useEffect(() => {
+    console.log("mounted component")
+
+    return () => {
+      console.log("dismounted component") // Se va a ejecutar cuando el componente se desmonte
+    }
+  }, [])
+
+
+  // El efecto se va a ejecutar cada vez que se modifique el estado del contador
+  useEffect(() => {
+    console.log("updated component")
+  }, [count])
+
+
   return (
-    <div>
-      <p>You clicked <p style={{fontSize:"100px"}}>{count}</p> times</p>
+    <>
+      <div style={{ fontSize: "20px" }}>You clicked <p style={{ fontSize: "100px" }}>{count}</p> times</div>
       <Button variant="primary" className="btn my-5" onClick={handleClick}>
         Click me
       </Button>
-      <Button variant="danger" onClick={resetCount}>
+      {
+        count !== 0 ?
+        <Button variant="danger" onClick={resetCount}>
         Reset
       </Button>
-    </div>
+        :
+        <div></div>
+      }
+
+      <p style={{ fontSize: "15px" }}>Last click: {new Date().toLocaleString()}</p>
+    </>
   );
 };
 
